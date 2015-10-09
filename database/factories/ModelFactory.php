4d37c08514447c75faa28c0e40bfcd2d52967387
Alex Oliveira <bsi.alexoliveira@gmail.com>
2015-10-09 13:01:44 -0300

    pequena correção

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -6,7 +6,7 @@ $factory->define(App\Models\User::class, function (Faker\Generator $faker) {
         'nickname' => $faker->firstName,
         'email' => $faker->email,
         'password' => bcrypt(str_random(10)),
-        'image' => $faker->image(250, 250),
+        'image' => $faker->imageUrl(250, 250),
         'birth' => $faker->dateTime,
         'remember_token' => str_random(10),
     ];
